9d7962dfea21d64bfec9aecfe4a0ffc630bcd9f7
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-06 16:21:12 +0530

    First Commit

---
+++ SchoolCrud_MVCproject2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolCrud_MVCproject2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SchoolCrud_MVCproject2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchoolCrud_MVCproject2.MvcApplication" Language="C#" %>
